d3487483f2cb50a0648f9b961d8d9f3310a49ce2
Pushkar Verma <pushkar7397@gmail.com>
2022-12-27 22:59:18 +0530

    Status codes and exceptions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -33,7 +33,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED) # add status code to the decorator for default values
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10000000)
@@ -41,7 +41,12 @@ def create_posts(post: Post):
     return {"data": post_dict} # return the data
 
 @app.get("/posts/{id}") # path parameter
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
-    print(post)
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found!")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with id: {id} was not found!!"}
+
     return {"post_details": post}
