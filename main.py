2e864c7b88f8ca66ab7c7d2e8e885d670b679b24
Pushkar Verma <pushkar7397@gmail.com>
2022-12-27 12:00:34 +0530

    Complete pydantic model for posts

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -9,6 +10,7 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
+    rating: Optional[int] = None # Optional Field Defaults to None
 
 @app.get("/")
 def root():
@@ -20,7 +22,8 @@ def get_posts():
 
 @app.post("/createposts")
 # import Body to fetch the data from the request body
-def create_posts(new_post: Post): # ... means that the data is required to be passed
-    print(new_post.published)
-    return {"data": "new post"} # return the data
+def create_posts(post: Post): # ... means that the data is required to be passed
+    print(post)
+    print(post.dict())
+    return {"data": post} # return the data
 # title str, content str
