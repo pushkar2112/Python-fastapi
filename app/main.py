0d1b1f6de5a735028b11a74f7412d2bed82770f0
Pushkar Verma <pushkar7397@gmail.com>
2022-12-29 18:59:30 +0530

    Establish DB Conn & Modify getall and create post

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
@@ -12,6 +15,18 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None # Optional Field Defaults to None
+
+# DB Connection
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='<PASSWORD>', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print('Database connection was successful!')
+        break
+    except Exception as error:
+        print("Connection failed!")
+        print('ERROR: ', error)
+        time.sleep(2)
 
 # Temporary post storage
 my_posts = [
@@ -31,14 +46,18 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute('''Select * from posts''')
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED) # add status code to the decorator for default values
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 10000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict} # return the data
+    # DO NOT USE FSTRINGS: they make us vulnerable to SQL Injection attacks
+    cursor.execute("""Insert into posts (title, content, published, owner_id) values (%s, %s, %s, 11) returning * """,(post.title, post.content, post.published))
+
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post} # return the data
 
 @app.get("/posts/{id}") # path parameter
 def get_post(id: int, response: Response):
