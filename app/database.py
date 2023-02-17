1971fb0ee22c78e89b9f19142ff51b106eecb063
Pushkar Verma <pushkar7397@gmail.com>
2023-02-17 15:35:48 +0530

    Code cleanup

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@<ip-address/hostname>/<database_name>"
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Pushkar1@localhost/fastapi"
@@ -19,3 +22,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# DB Connection
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='Pushkar1', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print('Database connection was successful!')
+#         break
+#     except Exception as error:
+#         print("Connection failed!")
+#         print('ERROR: ', error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,43 +1,11 @@
-from typing import Optional, List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-# DB Connection
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='Pushkar1', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print('Database connection was successful!')
-        break
-    except Exception as error:
-        print("Connection failed!")
-        print('ERROR: ', error)
-        time.sleep(2)
-
-# Temporary post storage
-my_posts = [
-    {"title": "title of post1", "content": "content of post1", "id": 1},
-    {"title": "title of post2", "content": "content of post2", "id": 2}
-    ]
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
 
 # Api routes
 app.include_router(post.router)
